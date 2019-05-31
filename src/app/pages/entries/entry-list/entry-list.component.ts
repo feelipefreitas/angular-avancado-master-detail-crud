400a27ae650f4b437a73862ea4ba23b58a3d088a
feelipefreitas <feelipe.freitas97@gmail.com>
2019-05-31 15:07:11 -0300

    Sorting entries

--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -16,7 +16,7 @@ export class EntryListComponent implements OnInit {
 
   ngOnInit() {
     this.entryService.getAll().subscribe(
-      (entries: Entry[]) => this.entries = entries,
+      (entries: Entry[]) => this.entries = entries.sort((a, b) => b.id - a.id),
       error => alert('Erro ao carregar a lista')
     );
   }
